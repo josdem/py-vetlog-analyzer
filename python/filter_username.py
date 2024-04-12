e49e2ec934f0f168d1afa066dd8ca8d054dbd6b1
josdem <joseluis.delacruz@gmail.com>
2024-04-12 19:50:02 -0400

    Staring a new Python data analysis project :)

---
+++ python/filter_username.py
@@ -0,0 +1,2 @@
+def filter_username(username):
+    return username == "josdem"
---
+++ test_filter_username.py
@@ -0,0 +1,13 @@
+import unittest
+from python.filter_username import *
+
+class FixedTest(unittest.TestCase):
+    def test_filter_usname(self):
+        self.assertTrue(filter_username("josdem"))
+        self.assertTrue(filter_username('johndoe'))
+        self.assertFalse(filter_username('NHUQfuLarRMDj'))
+        self.assertFalse(filter_username('rJVyFMNsmXhPUvG'))
+        
+
+if __name__ == '__main__':
+    unittest.main()        
