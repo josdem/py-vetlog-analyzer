f6d7ac07dffa5cf6a592f35741e3d6ea8e5dd5e1
josdem <joseluis.delacruz@gmail.com>
2024-04-13 11:22:59 -0400

    Filtering not valid users by username

--- test_database_connection.py
+++ test_database_connection.py
@@ -1,10 +1,12 @@
 import unittest
 
 from python.database_connection import *
+from python.filter_username import *
 
 class FixedTest(unittest.TestCase):
     def test_connect(self):
         self.assertIsNotNone(connect())
+        self.assertTrue(filter_usernames() >= 0)
 
 if __name__ == '__main__':
     unittest.main()        
