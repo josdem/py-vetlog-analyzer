31ce8ef5838209b0fc22a4358d8686433982d2ee
josdem <joseluis.delacruz@gmail.com>
2024-06-22 11:14:12 -0400

    #23 Adding suspicious filter test

--- py_vetlog_analyzer/suspicious_username.py
+++ py_vetlog_analyzer/suspicious_username.py
@@ -3,4 +3,4 @@ def is_suspicious_username(username: str) -> bool:
     for char in username:
         if char.isupper():
             uppercase += 1
-    return 0.1 < uppercase / len(username) <= 0.5
+    return 0.2 < uppercase / len(username) <= 0.5
--- tests/test_database_filter.py
+++ tests/test_database_filter.py
@@ -2,12 +2,14 @@ import unittest
 
 from py_vetlog_analyzer.database_filter import *
 from py_vetlog_analyzer.database_connector import *
+from py_vetlog_analyzer.suspicious_username import *
 
 
 class FixedTest(unittest.TestCase):
     def test_connect(self):
         self.assertIsNotNone(Connector().get_connector())
         self.assertTrue(Filter().filter_usernames() >= 0)
+        self.assertTrue(Filter().suspicious_usernames() >= 0)
 
 
 if __name__ == "__main__":
