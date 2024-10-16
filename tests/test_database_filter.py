e903bc8588825ed42e81fed83e8e5f28f4450eb7
josdem <joseluis.delacruz@gmail.com>
2024-10-16 06:19:56 -0600

    #30 Using greater or equal assertion instead

--- tests/test_database_filter.py
+++ tests/test_database_filter.py
@@ -6,8 +6,8 @@ from py_vetlog_analyzer.database_connector import Connector
 class FixedTest(unittest.TestCase):
     def test_connect(self):
         self.assertIsNotNone(Connector().get_connector())
-        self.assertTrue(Filter().filter_usernames() >= 0)
-        self.assertTrue(Filter().suspicious_usernames() >= 0)
+        self.assertGreaterEqual(Filter().filter_usernames(), 0)
+        self.assertGreaterEqual(Filter().suspicious_usernames(), 0)
 
 if __name__ == "__main__":
     unittest.main()
