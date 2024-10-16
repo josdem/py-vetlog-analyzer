127d8951031e283c98f523a8e639f6a38bdda95f
josdem <joseluis.delacruz@gmail.com>
2024-10-16 15:07:37 -0600

    Listing all pets waiting for vaccines

--- py_vetlog_analyzer/database_generate_vaccines.py
+++ py_vetlog_analyzer/database_generate_vaccines.py
@@ -6,10 +6,22 @@ class VaccinesGenerator:
         self.cursor = self.connection.cursor()
 
     def listing_pets(self):
-        count = 0
         print("Finding alls pets with vaccines in PENDING status")
-        self.cursor.execute("select pet.id, pet.name, vaccination.status from pet, vaccination where vaccination.pet_id = pet.id and vaccination.status='PENDING' group by pet.id")
+        self.cursor.execute("select pet.id, pet.name from pet, vaccination where vaccination.pet_id = pet.id and vaccination.status='PENDING' group by pet.id")
         result = self.cursor.fetchall()
         print("Pets found: ", len(result))
+        self.cursor.execute("select pet.id, pet.name from pet")
+        pets = self.cursor.fetchall()
+        print("Total Pets: ", len(pets))
+        print("Finding pets need vaccines")
+        pets_waiting_for_vaccines = [n for n in pets if n not in result]
+        for row in pets_waiting_for_vaccines:
+            print(
+                "id",
+                row[0],
+                "name:",
+                row[1]
+        )
+        print("Suspicious users found: ", len(pets_waiting_for_vaccines))
         self.connection.close()
-        return len(result)
+        return len(pets) - len(result)
