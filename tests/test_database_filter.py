b8a3a8a152eb6194fb335d3f25560801ca76e213
josdem <joseluis.delacruz@gmail.com>
2024-10-16 06:28:44 -0600

    #30 Formatting code

--- tests/test_database_filter.py
+++ tests/test_database_filter.py
@@ -3,11 +3,13 @@ import unittest
 from py_vetlog_analyzer.database_filter import Filter
 from py_vetlog_analyzer.database_connector import Connector
 
+
 class FixedTest(unittest.TestCase):
     def test_connect(self):
         self.assertIsNotNone(Connector().get_connector())
         self.assertGreaterEqual(Filter().filter_usernames(), 0)
         self.assertGreaterEqual(Filter().suspicious_usernames(), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
