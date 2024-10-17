e4228e71dd62081b4bf86611b4b220c1a26ae209
josdem <joseluis.delacruz@gmail.com>
2024-10-17 10:13:25 -0600

    #27 Generating vaccines as logs based on pet's birthdate

--- py_vetlog_analyzer/database_vaccines_generator.py
+++ py_vetlog_analyzer/database_vaccines_generator.py
@@ -1,4 +1,5 @@
 from py_vetlog_analyzer.database_connector import Connector
+from datetime import datetime
 
 
 class VaccinesGenerator:
@@ -7,5 +8,32 @@ class VaccinesGenerator:
         self.cursor = self.connection.cursor()
 
     def generateVaccines(pet):
+        now = datetime.now()
+        weeks = (now - pet[2]).days / 7
+        print("weeks", int(weeks))
+
+        match int(weeks):
+            case 4:
+                print("Vaccine 1: Parvovirus")
+                print("Vaccine 2: Distemper")
+                print("Vaccine 3: Hepatitis")
+            case 6:
+                print("Vaccine 1: Parvovirus")
+                print("Vaccine 2: Distemper")
+                print("Vaccine 3: Hepatitis")
+                print("Vaccine 4: Leptospirosis")
+            case 8:
+                print("Vaccine 1: Parvovirus")
+                print("Vaccine 2: Distemper")
+                print("Vaccine 3: Hepatitis")
+                print("Vaccine 4: Leptospirosis")
+                print("Vaccine 5: Parainfluenza")
+            case 10:
+                print("Vaccine 1: Parvovirus")
+                print("Vaccine 2: Distemper")
+                print("Vaccine 3: Hepatitis")
+                print("Vaccine 4: Leptospirosis")
+                print("Vaccine 5: Parainfluenza")
+
         print("id", pet[0], "name:", pet[1], "birthdate:", pet[2], "type:", pet[3])
         return pet
