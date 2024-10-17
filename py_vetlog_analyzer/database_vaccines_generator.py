a27bb649aa70c814715a2dadd0841709f74061e8
josdem <joseluis.delacruz@gmail.com>
2024-10-17 12:22:05 -0600

    #27 Introducing local method

--- py_vetlog_analyzer/database_vaccines_generator.py
+++ py_vetlog_analyzer/database_vaccines_generator.py
@@ -8,31 +8,33 @@ class VaccinesGenerator:
         self.cursor = self.connection.cursor()
 
     def generateVaccines(pet):
-        if(pet[3] != "DOG"):
+        pet = pet
+        if pet[3] != "DOG":
             return pet
         now = datetime.now()
         weeks = (now - pet[2]).days / 7
         print("weeks", int(weeks))
 
+        def registerVaccination(name):
+            print("Registering vaccination", name)
+            print("id", pet[0], "name:", pet[1], "birthdate:", pet[2], "type:", pet[3])
+
         match int(weeks):
             case weeks if weeks in range(6, 9):
-                print("Vaccine 1: DA2PP")
-                print("Vaccine 2: Deworming")
+                registerVaccination("DA2PP")
+                registerVaccination("Deworming")
             case weeks if weeks in range(10, 13):
-                print("Vaccine 1: DA2PP")
-                print("Vaccine 2: Deworming")
-                print("Vaccine 4: Leptospirosis")
+                registerVaccination("DA2PP")
+                registerVaccination("Deworming")
+                registerVaccination("Leptospirosis")
             case weeks if weeks in range(14, 16):
-                print("Vaccine 1: DA2PP")
-                print("Vaccine 2: Deworming")
-                print("Vaccine 4: Leptospirosis")
-                print("Vaccine 5: Rabies")
+                registerVaccination("DA2PP")
+                registerVaccination("Deworming")
+                registerVaccination("Leptospirosis")
+                registerVaccination("Rabies")
             case weeks if weeks >= 17:
-                print("Vaccine 1: DA2PP")
-                print("Vaccine 2: Deworming")
-                print("Vaccine 4: Leptospirosis")
-                print("Vaccine 5: Rabies")
-                print("Vaccine 5: Canine Influenza")
-
-        print("id", pet[0], "name:", pet[1], "birthdate:", pet[2], "type:", pet[3])
-        return pet
+                registerVaccination("DA2PP")
+                registerVaccination("Deworming")
+                registerVaccination("Leptospirosis")
+                registerVaccination("Rabies")
+                registerVaccination("Canine Influenza")
