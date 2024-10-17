bc0ce287f69ada7e13394e239a8b8c26e5b621bc
josdem <joseluis.delacruz@gmail.com>
2024-10-17 05:12:39 -0600

    #27 Adding basic test

---
+++ tests/test_database_filter_pets.py
@@ -0,0 +1,11 @@
+import unittest
+
+from py_vetlog_analyzer.database_filter_pets import PetFilter
+
+
+class FixedTest(unittest.TestCase):
+    def test_list_pets(self):
+        self.assertGreater(len(PetFilter().filtering_pets()), 0)
+
+if __name__ == "__main__":
+    unittest.main()
