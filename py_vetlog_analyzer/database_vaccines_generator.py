37dc7fb4127b2aed7299beb67bb794f1b97860bd
josdem <joseluis.delacruz@gmail.com>
2024-10-17 11:38:26 -0600

    #27 Adding ranges based on weeks

--- py_vetlog_analyzer/database_vaccines_generator.py
+++ py_vetlog_analyzer/database_vaccines_generator.py
@@ -8,32 +8,31 @@ class VaccinesGenerator:
         self.cursor = self.connection.cursor()
 
     def generateVaccines(pet):
+        if(pet[3] != "DOG"):
+            return pet
         now = datetime.now()
         weeks = (now - pet[2]).days / 7
         print("weeks", int(weeks))
 
         match int(weeks):
-            case 4:
-                print("Vaccine 1: Parvovirus")
-                print("Vaccine 2: Distemper")
-                print("Vaccine 3: Hepatitis")
-            case 6:
-                print("Vaccine 1: Parvovirus")
-                print("Vaccine 2: Distemper")
-                print("Vaccine 3: Hepatitis")
+            case weeks if weeks in range(6, 9):
+                print("Vaccine 1: DA2PP")
+                print("Vaccine 2: Deworming")
+            case weeks if weeks in range(10, 13):
+                print("Vaccine 1: DA2PP")
+                print("Vaccine 2: Deworming")
                 print("Vaccine 4: Leptospirosis")
-            case 8:
-                print("Vaccine 1: Parvovirus")
-                print("Vaccine 2: Distemper")
-                print("Vaccine 3: Hepatitis")
+            case weeks if weeks in range(14, 16):
+                print("Vaccine 1: DA2PP")
+                print("Vaccine 2: Deworming")
                 print("Vaccine 4: Leptospirosis")
-                print("Vaccine 5: Parainfluenza")
-            case 10:
-                print("Vaccine 1: Parvovirus")
-                print("Vaccine 2: Distemper")
-                print("Vaccine 3: Hepatitis")
+                print("Vaccine 5: Rabies")
+            case weeks if weeks >= 17:
+                print("Vaccine 1: DA2PP")
+                print("Vaccine 2: Deworming")
                 print("Vaccine 4: Leptospirosis")
-                print("Vaccine 5: Parainfluenza")
+                print("Vaccine 5: Rabies")
+                print("Vaccine 5: Canine Influenza")
 
         print("id", pet[0], "name:", pet[1], "birthdate:", pet[2], "type:", pet[3])
         return pet
