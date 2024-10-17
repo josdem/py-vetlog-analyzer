a9fae3b8f0deb03c42a185bc3e45a2217119b3f7
josdem <joseluis.delacruz@gmail.com>
2024-10-17 15:34:23 -0600

    #27 Updating assertion

--- tests/test_database_filter_pets.py
+++ tests/test_database_filter_pets.py
@@ -5,7 +5,7 @@ from py_vetlog_analyzer.database_filter_pets import PetFilter
 
 class FixedTest(unittest.TestCase):
     def test_list_pets(self):
-        self.assertGreater(len(PetFilter().filtering_pets()), 0)
+        self.assertGreaterEqual(len(PetFilter().filtering_pets()), 0)
 
 
 if __name__ == "__main__":
