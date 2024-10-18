c29551147d65d514f71f0253a45e65887c3752b4
josdem <joseluis.delacruz@gmail.com>
2024-10-18 12:54:18 -0600

    #29 Adding database vaccines gneration test

---
+++ tests/test_database_vaccines_generator.py
@@ -0,0 +1,18 @@
+import unittest
+import datetime
+
+from py_vetlog_analyzer.database_vaccines_generator import VaccinesGenerator
+
+
+class FixedTest(unittest.TestCase):
+    def test_generate_first_vaccination_records(self):
+        pet = ["1", "Cremita", datetime.datetime(2024, 8, 23), "DOG"]
+        self.assertEqual(VaccinesGenerator().generate_vaccines(pet), 2)
+
+    def test_generate_second_vaccination_records(self):
+        pet = ["1", "Cremita", datetime.datetime(2024, 8, 3), "DOG"]
+        self.assertEqual(VaccinesGenerator().generate_vaccines(pet), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
