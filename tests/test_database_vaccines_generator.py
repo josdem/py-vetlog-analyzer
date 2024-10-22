fea0513b1ab2df2cfad76e1f544df106448a60bf
josdem <joseluis.delacruz@gmail.com>
2024-10-22 06:14:42 -0600

    #45 Adding pet's constant to avoid using a string

--- tests/test_database_vaccines_generator.py
+++ tests/test_database_vaccines_generator.py
@@ -1,24 +1,27 @@
+from typing import Final
 import unittest
 import datetime
 
 from py_vetlog_analyzer.database_vaccines_generator import VaccinesGenerator
 
+PET_TYPE: Final[str] = "DOG"
+
 
 class FixedTest(unittest.TestCase):
     def test_generate_first_vaccination_records(self):
-        pet = ["1", "Cremita", datetime.datetime(2024, 8, 23), "DOG"]
+        pet = ["1", "Cremita", datetime.datetime(2024, 8, 23), PET_TYPE]
         self.assertEqual(VaccinesGenerator().generate_vaccines(pet), 2)
 
     def test_generate_second_vaccination_records(self):
-        pet = ["1", "Cremita", datetime.datetime(2024, 8, 3), "DOG"]
+        pet = ["1", "Cremita", datetime.datetime(2024, 8, 3), PET_TYPE]
         self.assertEqual(VaccinesGenerator().generate_vaccines(pet), 3)
 
     def test_generate_third_vaccination_records(self):
-        pet = ["1", "Cremita", datetime.datetime(2024, 7, 3), "DOG"]
+        pet = ["1", "Cremita", datetime.datetime(2024, 7, 3), PET_TYPE]
         self.assertEqual(VaccinesGenerator().generate_vaccines(pet), 4)
 
     def test_generate_annual_vaccination_records(self):
-        pet = ["1", "Cremita", datetime.datetime(2022, 10, 18), "DOG"]
+        pet = ["1", "Cremita", datetime.datetime(2022, 10, 18), PET_TYPE]
         self.assertEqual(VaccinesGenerator().generate_vaccines(pet), 5)
 
 
