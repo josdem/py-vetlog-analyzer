123b9a6e5fc78f6dd172c8c99fe387f48a292e2c
josdem <joseluis.delacruz@gmail.com>
2024-10-23 06:28:09 -0600

    #46 Using logger for vaccines

--- py_vetlog_analyzer/database_vaccines_generator.py
+++ py_vetlog_analyzer/database_vaccines_generator.py
@@ -1,22 +1,25 @@
 from py_vetlog_analyzer.database_connector import Connector
 from datetime import datetime
+from py_vetlog_analyzer.logger import Logger
 
 
 class VaccinesGenerator:
+
+    def __init__(self):
+        self.logger = Logger("VaccinesGenerator")
 
     def generate_vaccines(self, pet):
         count = 0
         if pet[3] != "DOG":
             return pet
+        self.logger.info("Registering vaccination for pet: %s", pet[1])
         now = datetime.now()
         weeks = (now - pet[2]).days / 7
-        print("weeks", int(weeks))
+        self.logger.info("weeks: %d", int(weeks))
 
         def register_vaccination(name):
             connection = Connector().get_connector()
             cursor = connection.cursor()
-
-            print("id", pet[0], "name:", pet[1], "birthdate:", pet[2], "type:", pet[3])
 
             cursor.execute(
                 "INSERT INTO vaccination (pet_id, name, date, status) VALUES (%s, %s, %s, 'PENDING')",
