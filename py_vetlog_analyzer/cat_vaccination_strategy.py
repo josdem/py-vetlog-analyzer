16dd65f6b1b7090a46569eb604ebaf134a79a272
josdem <joseluis.delacruz@gmail.com>
2024-10-24 10:19:36 -0600

    Adding interface implementations

---
+++ py_vetlog_analyzer/cat_vaccination_strategy.py
@@ -0,0 +1,7 @@
+from py_vetlog_analyzer.vaccination_strategy import VaccinationStrategy
+
+
+class CatVaccinationStrategy(VaccinationStrategy):
+
+    def vaccinate(self, pet):
+        return pet
---
+++ py_vetlog_analyzer/dog_vaccination_strategy.py
@@ -0,0 +1,7 @@
+from py_vetlog_analyzer.vaccination_strategy import VaccinationStrategy
+
+
+class DogVaccinationStrategy(VaccinationStrategy):
+
+    def vaccinate(self, pet):
+        return pet
