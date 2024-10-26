58fa80fb5bbb6fb43e971640b83768a4a524181f
josdem <joseluis.delacruz@gmail.com>
2024-10-26 05:00:40 -0600

    #42 Generating dog vaccination logic

--- py_vetlog_analyzer/dog_vaccination_strategy.py
+++ py_vetlog_analyzer/dog_vaccination_strategy.py
@@ -1,5 +1,40 @@
 from py_vetlog_analyzer.vaccination_strategy import VaccinationStrategy
+from py_vetlog_analyzer.logger import Logger
+from datetime import datetime
 
 class DogVaccinationStrategy(VaccinationStrategy):
-    def vaccinate(self, pet):
-        return pet
+    
+    def __init__(self):
+        self.logger = Logger("DogVaccinationStrategy")
+        
+    def generate_vaccines(self, pet):
+        self.logger.info("Registering vaccination for pet: %s", pet[1])
+        now = datetime.now()
+        weeks = (now - pet[2]).days / 7
+        self.logger.info("Pet is %d weeks old", int(weeks))
+        def register_vaccination(name):
+            self.logger.info("Generating %s vaccination", name)
+        match int(weeks):
+            case weeks if weeks in range(6, 10):
+                register_vaccination("DA2PP")
+                register_vaccination("Deworming")
+                count = 2
+            case weeks if weeks in range(10, 14):
+                register_vaccination("DA2PP")
+                register_vaccination("Deworming")
+                register_vaccination("Leptospirosis")
+                count = 3
+            case weeks if weeks in range(14, 17):
+                register_vaccination("DA2PP")
+                register_vaccination("Deworming")
+                register_vaccination("Leptospirosis")
+                register_vaccination("Rabies")
+                count = 4
+            case weeks if weeks >= 17:
+                register_vaccination("DA2PP")
+                register_vaccination("Deworming")
+                register_vaccination("Leptospirosis")
+                register_vaccination("Rabies")
+                register_vaccination("Canine Influenza")
+                count = 5
+        return count
