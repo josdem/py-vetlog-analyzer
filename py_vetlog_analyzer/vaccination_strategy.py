b265f6ab3554eda906c634d39eccdb737e2ee273
josdem <joseluis.delacruz@gmail.com>
2024-10-26 05:03:33 -0600

    #42 Renaming method

--- py_vetlog_analyzer/vaccination_strategy.py
+++ py_vetlog_analyzer/vaccination_strategy.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 class VaccinationStrategy(ABC):
+
     @abstractmethod
-    def vaccinate(self, pet):
+    def generate_vaccines(self, pet):
         return pet
