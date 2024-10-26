e63fdfa9fcaa1ca7a9a46aaf310522045e9c6e40
josdem <joseluis.delacruz@gmail.com>
2024-10-26 05:01:23 -0600

    #42 Generating cat vaccination logic

--- py_vetlog_analyzer/cat_vaccination_strategy.py
+++ py_vetlog_analyzer/cat_vaccination_strategy.py
@@ -1,5 +1,27 @@
 from py_vetlog_analyzer.vaccination_strategy import VaccinationStrategy
+from py_vetlog_analyzer.logger import Logger
+from datetime import datetime
 
 class CatVaccinationStrategy(VaccinationStrategy):
-    def vaccinate(self, pet):
-        return pet
+    
+    def __init__(self):
+        self.logger = Logger("CatVaccinationStrategy")
+        
+    def generate_vaccines(self, pet):
+        self.logger.info("Registering vaccination for pet: %s", pet[1])
+        now = datetime.now()
+        weeks = (now - pet[2]).days / 7
+        self.logger.info("Pet is %d weeks old", int(weeks))
+        def register_vaccination(name):
+            self.logger.info("Generating %s vaccination", name)
+        match int(weeks):
+            case weeks if weeks in range(8, 16):
+                register_vaccination("FVRCP")
+                register_vaccination("Deworming")
+                count = 1
+            case weeks if weeks >= 16:
+                register_vaccination("FVRCP")
+                register_vaccination("Deworming")
+                register_vaccination("Rabies")
+                count = 2
+        return count
