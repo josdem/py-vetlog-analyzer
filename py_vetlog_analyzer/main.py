054edbbdcf0c572312a4d4db289239ef43815129
josdem <joseluis.delacruz@gmail.com>
2024-12-13 14:02:30 -0600

    #54 Adding filter by name

--- py_vetlog_analyzer/main.py
+++ py_vetlog_analyzer/main.py
@@ -2,13 +2,14 @@ from py_vetlog_analyzer.database_filter import Filter
 from py_vetlog_analyzer.database_filter_pets import PetFilter
 
 
-def find():
-    Filter().filter_users()
+def flter_by_username():
+    Filter().filter_users(12)
 
+def flter_by_name():
+    Filter().filter_users(7)
 
 def suggest():
     Filter().suspicious_usernames()
-
 
 def vaccines():
     PetFilter().filtering_pets()
