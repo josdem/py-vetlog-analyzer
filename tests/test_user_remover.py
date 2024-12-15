723276796f264e94656d50008a865a5b4758318e
josdem <joseluis.delacruz@gmail.com>
2024-12-15 13:21:26 -0600

    #56 Removing extra parameter

--- tests/test_user_remover.py
+++ tests/test_user_remover.py
@@ -10,7 +10,7 @@ class FixedTest(unittest.TestCase):
         db_filter.cursor = MagicMock()
         db_filter.remove_user(409)
         db_filter.cursor.execute.assert_called_with(
-            "DELETE FROM user WHERE id = %d", (409,)
+            "DELETE FROM user WHERE id = %d", 409
         )
         db_filter.connection.commit.assert_called_once()
         db_filter.connection.close.assert_called_once()
