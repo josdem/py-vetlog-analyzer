714f67c558ec53fb22e9e18a0d19dcbc05696759
jgafnea <84107636+jgafnea@users.noreply.github.com>
2025-11-25 13:43:49 -0600

    test: redo migrations to pytest

--- tests/test_filter_username.py
+++ tests/test_filter_username.py
@@ -11,26 +11,25 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License
-
-import unittest
-
-from vetlog_buddy.filter_username import filter_username
+import pytest
+from py_vetlog_analyzer.filter_username import filter_username
 
 
-class FixedTest(unittest.TestCase):
-    def test_filter_usname(self):
-        factor = 0.5
-        self.assertTrue(filter_username("josdem", factor))
-        self.assertTrue(filter_username("johndoe", factor))
-        self.assertTrue(filter_username("IRIS", factor))
-        self.assertTrue(filter_username("Max", factor))
-        self.assertTrue(filter_username("Jc", factor))
-        self.assertFalse(filter_username("NHUQfuLarRMDj", factor))
-        self.assertFalse(filter_username("rJVyFMNsmXhPUvG", factor))
-        self.assertFalse(filter_username("rVhBLNPSNIPE", factor))
-        self.assertFalse(filter_username("SxeQsgXI", factor))
-        self.assertFalse(filter_username("NDDmMAUftYXkxO", factor))
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize(
+    "username,expected",
+    [
+        ("josdem", True),
+        ("johndoe", True),
+        ("IRIS", True),
+        ("Max", True),
+        ("Jc", True),
+        ("NHUQfuLarRMDj", False),
+        ("rJVyFMNsmXhPUvG", False),
+        ("rVhBLNPSNIPE", False),
+        ("SxeQsgXI", False),
+        ("NDDmMAUftYXkxO", False),
+    ],
+)
+def test_filter_username(username, expected):
+    factor = 0.5
+    assert filter_username(username, factor) == expected
