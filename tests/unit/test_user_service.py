021a1be7331f5fdad0e55c22acf13e67dde4e88b
jgafnea <84107636+jgafnea@users.noreply.github.com>
2025-12-01 11:57:19 -0600

    test: update user unit tests for refactored services
    combined tests from:
    - tests/test_filter_username.py
    - tests/test_suspicious_username.py
    
    now located in:
    - tests/unit/test_user_services.py

---
+++ tests/unit/test_user_service.py
@@ -0,0 +1,62 @@
+import pytest
+
+from vetlog_buddy.users.models import User
+from vetlog_buddy.users.repository import UserRepository
+from vetlog_buddy.users.services import UserService
+
+
+class DummyRepo(UserRepository):
+    def __init__(self):
+        pass
+
+
+"""
+Note parametrize accepts any of these
+
+    "username,expected"
+    ("username", "expected")
+    ["username", "expected"]
+
+Linters seem to prefer tuple format
+"""
+
+
+@pytest.mark.parametrize(
+    ("username", "expected"),
+    [
+        ("PvbGzTHuyk", True),
+        ("otzUnBpWKQj", True),
+        ("dfLybkwvMBrtWcY", True),
+        ("qIiaPgOoH", True),
+        ("simonhodgson3237@icloud.com", False),
+    ],
+)
+def test_is_suspicious(username, expected):
+    """Confirm UserService marks usernames correctly as suspicious"""
+    # usernames from test_suspicious_username.py
+    service = UserService(repo=DummyRepo())
+    user = User(username=username)
+    assert service.is_suspicious(user) == expected
+
+
+@pytest.mark.parametrize(
+    ("username", "expected"),
+    [
+        ("josdem", True),
+        ("johndoe", True),
+        ("IRIS", True),
+        ("Max", True),
+        ("Jc", True),
+        ("NHUQfuLarRMDj", False),
+        ("rJVyFMNsmXhPUvG", False),
+        ("rVhBLNPSNIPE", False),
+        ("SxeQsgXI", False),
+        ("NDDmMAUftYXkxO", False),
+    ],
+)
+def test_is_invalid(username, expected):
+    """Confirm UserService marks usernames correctly as invalid"""
+    # usernames from test_filter_username.py
+    service = UserService(repo=DummyRepo())
+    user = User(username=username)
+    assert service.is_invalid(user) == expected
